1e511156c3e2aef31c40e425ecc88ae41e9daa06
Brady Anderson <anderbradyw@gmail.com>
2017-02-11 11:19:20 -0600

    Implements LS1 and tested it using the example from appendix G

--- Key.cpp
+++ Key.cpp
@@ -6,20 +6,9 @@
 
 // Define parallel arrays of bit masks and shift amounts used to permute the 10 bit key
 
-class BitMasks {
-public:
-    static const int masks[];
-};
+const int Key::masks[] = {0x000, 0x200, 0x100, 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1};
 
-const int BitMasks::masks[] = {0x000, 0x200, 0x100, 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1};
-
-
-class PermuteTenShifts {
-public:
-    static const int shifts[];
-};
-
-const int PermuteTenShifts::shifts[] = {0, 6, 1, -2, 1, -4, -3, 1, -2, -1, -4};
+const int Key::pTenShifts[] = {0, 6, 1, -2, 1, -4, -3, 1, -2, -1, -4};
 
 Key::Key(unsigned int seed) {
 
@@ -47,23 +36,52 @@ unsigned int Key::getKeyTwo() {
 void Key::generateSubKeys() {
 
     permuteTen();
+    leftShiftOne();
 
 }
 
 void Key::permuteTen() {
 
-    unsigned int firstPermutaion = 0;
     unsigned int temp = keyInput;
     for (int i = 0; i < 11; i++) {
-        temp = keyInput & BitMasks::masks[i];
-        if (PermuteTenShifts::shifts[i] > 0)
-            temp = temp >> PermuteTenShifts::shifts[i];
+        temp = keyInput & masks[i];
+        if (pTenShifts[i] > 0)
+            temp = temp >> pTenShifts[i];
         else
-            temp = temp << -(PermuteTenShifts::shifts[i]);
-        firstPermutaion = firstPermutaion | temp;
+            temp = temp << -(pTenShifts[i]);
+        key10bit = key10bit | temp;
     }
 
-    std::cout << "The result of P10 is: " << firstPermutaion << std::endl;
+    //std::cout << "The result of P10 is: " << key10bit << std::endl;
+
+}
+
+void Key::leftShiftOne() {
+
+    unsigned int right5bits = 0;
+    unsigned int left5bits = 0;
+
+    right5bits = key10bit & rightBitMask;
+    left5bits = key10bit & leftBitMask;
+
+    right5bits = right5bits << 1;
+    left5bits = left5bits << 1;
+
+    if (left5bits & 0x400) {
+        left5bits = left5bits | 0x20;
+        left5bits = left5bits & leftBitMask;
+    }
+
+    if (right5bits & 0x20) {
+        right5bits = right5bits | 1;
+        right5bits = right5bits & rightBitMask;
+    }
+
+    key10bit = 0;
+    key10bit = key10bit | right5bits;
+    key10bit = key10bit | left5bits;
+
+    std::cout << "After LS1 key10bit is: " << key10bit << std::endl;
 
 }
 
--- Key.h
+++ Key.h
@@ -14,12 +14,17 @@ public:
 
 private:
 
+    static const int masks[];
+    static const int pTenShifts[];
+    static const int rightBitMask = 0x1F;
+    static const int leftBitMask = 0x3E0;
     unsigned int keyInput;
     unsigned int key10bit;
     unsigned int keyOne;
     unsigned int keyTwo;
     void generateSubKeys();
     void permuteTen();
+    void leftShiftOne();
 
 
 };
