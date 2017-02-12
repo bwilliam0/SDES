f445519a10875be26d1a06ce3f094164acb03922
Brady Anderson <anderbradyw@gmail.com>
2017-02-12 00:42:58 -0600

    Implemented P8 and tested it with the data from appendix G

--- Key.cpp
+++ Key.cpp
@@ -6,9 +6,13 @@
 
 // Define parallel arrays of bit masks and shift amounts used to permute the 10 bit key
 
-const int Key::masks[] = {0x000, 0x200, 0x100, 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1};
+const unsigned int Key::masks[] = {0x200, 0x100, 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1};
 
-const int Key::pTenShifts[] = {0, 6, 1, -2, 1, -4, -3, 1, -2, -1, -4};
+const int Key::pTenShifts[] = {6, 1, -2, 1, -4, -3, 1, -2, -1, -4};
+
+const int Key::pEightShifts[] = {-3, 1, -2, 2, -1, 3, -1, 1};
+
+const unsigned int Key::pEightMasks[] = {0x10, 0x80, 0x08, 0x40, 0x04, 0x20, 0x01, 0x02};
 
 Key::Key(unsigned int seed) {
 
@@ -37,12 +41,13 @@ void Key::generateSubKeys() {
 
     permuteTen();
     leftShiftOne();
+    keyOne = permuteEight(key10bit);
 
 }
 
 void Key::permuteTen() {
 
-    unsigned int temp = keyInput;
+    unsigned int temp;
     for (int i = 0; i < 11; i++) {
         temp = keyInput & masks[i];
         if (pTenShifts[i] > 0)
@@ -52,7 +57,7 @@ void Key::permuteTen() {
         key10bit = key10bit | temp;
     }
 
-    //std::cout << "The result of P10 is: " << key10bit << std::endl;
+    std::cout << "The result of P10 is: " << key10bit << std::endl;
 
 }
 
@@ -82,6 +87,23 @@ void Key::leftShiftOne() {
     key10bit = key10bit | left5bits;
 
     std::cout << "After LS1 key10bit is: " << key10bit << std::endl;
+
+}
+
+int Key::permuteEight(int inputKey) {
+
+    int outputKey = 0;
+    unsigned int temp;
+    for (int i = 0; i < 8; i++) {
+        temp = key10bit & pEightMasks[i];
+        if (pEightShifts[i] > 0)
+            temp = temp >> pEightShifts[i];
+        else
+            temp = temp << -pEightShifts[i];
+        outputKey = outputKey | temp;
+    }
+
+    std::cout << "The result of P8 is: " << outputKey << std::endl;
 
 }
 
--- Key.h
+++ Key.h
@@ -14,18 +14,20 @@ public:
 
 private:
 
-    static const int masks[];
-    static const int pTenShifts[];
-    static const int rightBitMask = 0x1F;
-    static const int leftBitMask = 0x3E0;
-    unsigned int keyInput;
-    unsigned int key10bit;
-    unsigned int keyOne;
-    unsigned int keyTwo;
+    static const unsigned int masks[];///< Defines a list of bit masks that can be used to pick out individual bits
+    static const int pTenShifts[];///< Defines a list of bit shifts used in the 10 bit permutation
+    static const int pEightShifts[];///< Defines a list of bit shifts used in the 8 bit permutation
+    static const unsigned int pEightMasks[];///< Defines a list of bit masks used in the 8 bit permutation
+    static const unsigned int rightBitMask = 0x1F;///< Bit mask used to pick out the right 5 bits in the ten bit key
+    static const unsigned int leftBitMask = 0x3E0;///< Bit mask used to pick out the left 5 bits in the ten bit key
+    unsigned int keyInput;///< Stores the key that was input by the user
+    unsigned int key10bit;///< Stores the 10 bit key after the multiple shifts
+    unsigned int keyOne;///< Stores the first 8 bit key
+    unsigned int keyTwo;///< Stores the second 8 bit key
     void generateSubKeys();
     void permuteTen();
     void leftShiftOne();
-
+    int permuteEight(int inputKey);
 
 };
 
