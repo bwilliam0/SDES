f0b2f1ff3eceb0beb45ac143b964c34a1736c13c
Brady Anderson <anderbradyw@gmail.com>
2017-02-12 18:06:58 -0600

    Added a new main function for testing the Key generator. Switched the return type of the subkey getters to bitset. Completed comments.

--- Key.h
+++ Key.h
@@ -2,32 +2,39 @@
 #define _KEY_H
 
 #include <iostream>
+#include <stdexcept>
 #include <bitset>
 
 class Key {
 
 public:
 
+    static const unsigned int RIGHT_BIT_MASK = 0x1F;///< Bit mask used to pick out the right 5 bits in the ten bit key
+    static const unsigned int LEFT_BIT_MASK = 0x3E0;///< Bit mask used to pick out the left 5 bits in the ten bit key
+    static const unsigned int KEY_MAX_SIZE = 1023;///< The maximum decimal value that the given key can be
+    static const unsigned int KEY_SIZE = 10;///< The number of bits in the key
+    static const unsigned int SUBKEY_SIZE = 8;///< The number of bits in the subkey
+
     Key(unsigned int seed);
-    unsigned int getKeyOne();
-    unsigned int getKeyTwo();
+    std::bitset<SUBKEY_SIZE> getKeyOne();
+    std::bitset<SUBKEY_SIZE> getKeyTwo();
 
 private:
 
+    static const unsigned int pEightMasks[];///< Defines a list of bit masks used in the 8 bit permutation
     static const unsigned int masks[];///< Defines a list of bit masks that can be used to pick out individual bits
     static const int pTenShifts[];///< Defines a list of bit shifts used in the 10 bit permutation
     static const int pEightShifts[];///< Defines a list of bit shifts used in the 8 bit permutation
-    static const unsigned int pEightMasks[];///< Defines a list of bit masks used in the 8 bit permutation
-    static const unsigned int rightBitMask = 0x1F;///< Bit mask used to pick out the right 5 bits in the ten bit key
-    static const unsigned int leftBitMask = 0x3E0;///< Bit mask used to pick out the left 5 bits in the ten bit key
+
     unsigned int keyInput;///< Stores the key that was input by the user
     unsigned int key10bit;///< Stores the 10 bit key after the multiple shifts
     unsigned int keyOne;///< Stores the first 8 bit key
     unsigned int keyTwo;///< Stores the second 8 bit key
+
     void generateSubKeys();
     void permuteTen();
     void leftShiftOne();
-    int permuteEight(unsigned int inputKey);
+    unsigned int permuteEight(unsigned int inputKey);
 
 };
 
---
+++ keytest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Key.h"
+using namespace std;
+
+int main() {
+
+    Key keygen(0x282);
+    Key keygen2(1023);
+
+    return 0;
+
+}
